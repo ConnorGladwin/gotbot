a83809b6f103e74ee73da20d33bb7cbe864a7517
ConnorGladwin <cygladwin795@gmail.com>
2024-03-14 13:51:18 +0200

    edit item

--- frontend/src/components/dashboard/EditItem.vue
+++ frontend/src/components/dashboard/EditItem.vue
@@ -6,7 +6,24 @@
         >Edit Item</span
       >
     </div>
+    <div
+      class="h-[500px] flex flex-col items-center gap-4 mt-8 overflow-y-scroll"
+    >
+      <div v-for="item in list" :key="item" class="w-[80%]">
+        <EditItemCard :item="item" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import { inventory } from "../../util/queries/food.js";
+import EditItemCard from "./EditItemCard.vue";
+
+const list = ref();
+
+onMounted(async () => {
+  list.value = await inventory();
+});
+</script>
